354964929a19ffe9e6ebec185f0e0d67dfc5d20a
Abhishek Bhave <abhishekbhave26@gmail.com>
2019-05-27 21:24:24 +0530

    Update abhaveLab1Part3 getTweets.R

--- Lab 1/Part 3/abhaveLab1Part3 getTweets.R
+++ Lab 1/Part 3/abhaveLab1Part3 getTweets.R
@@ -3,13 +3,6 @@
 library(rtweet)
 
 ## authenticate via access token
-token <- create_token(
-  app = "Twitter with R123",
-  consumer_key = "8InsjGGJGkPl1Kt2jrEoMm3BD",
-  consumer_secret = "SDVWDeItox0YLcnaa4TKKrqftDIWUxN6cvjqPtoSdk9QMUqS1C",
-  acess_token = "9551451262-wK2EmA942kxZYIwa5LMKZoQA4Xc2uyIiEwu2YXL",
-  access_secret = "9vpiSGKg1fIPQtxc5d5ESiFlZQpfbknEN1f1m2xe5byw7")
-
 
 list <-c('flu','fluseason','cough','influenza')
 for(i in list)
